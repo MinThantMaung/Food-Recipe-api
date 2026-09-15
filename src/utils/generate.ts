cd06651ee7699efc399331f9767b4a0672316bdc
Min Thant Maung <minthant180@gmail.com>
2026-09-15 17:47:34 +0900

    feat: add generateOtp func

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -3,3 +3,7 @@ import { randomBytes } from "crypto"
 export const generateToken = () => {
     return randomBytes(32).toString("hex");
 }
+
+export const generateOtpCode = Math.floor(
+  100000 + Math.random() * 900000,
+);
