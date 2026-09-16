9e754a2b436b0e300d24e9ec4790cea0b61b4b0c
Min Thant Maung <minthant180@gmail.com>
2026-09-16 13:36:26 +0900

    feat: create route for auth flow

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,15 +1,18 @@
 import express from 'express';
-import { confirmPassword, register, verifyOtp } from '../../controllers/authController';
+import { authCheck, confirmPassword, login, register, verifyOtp } from '../../controllers/authController';
+import { auth } from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/verify-otp', verifyOtp);
 router.post('/confirm-password', confirmPassword);
-// router.post('/login', login);
+router.post('/login', login);
 // router.post('/logout', logout);
 
 //google login route
 //router.post("/auth/google", googleLogin);
+
+router.get("/auth-check", auth, authCheck);
 
 export default router;
