2f67be2132b2bc34e5ebb411dce7f2613ce75abe
Min Thant Maung <minthant180@gmail.com>
2026-09-18 17:28:24 +0900

    feat:implement router for login and logout

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,14 +1,15 @@
 import express from 'express';
-import { authCheck, confirmPassword, login, register, verifyOtp } from '../../controllers/authController';
+import { authCheck, confirmPassword, login, logout, register, verifyOtp } from '../../controllers/authController';
 import { auth } from '../../middlewares/auth';
+import { loginLimiter } from '../../middlewares/rateLimiter';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/verify-otp', verifyOtp);
 router.post('/confirm-password', confirmPassword);
-router.post('/login', login);
-// router.post('/logout', logout);
+router.post('/login',loginLimiter, login);
+router.post('/logout', logout);
 
 //google login route
 //router.post("/auth/google", googleLogin);
